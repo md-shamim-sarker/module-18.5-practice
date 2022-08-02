6df2b23eb78a518b33c42db63978533b41f79057
shamimspro <shamimspro@gmail.com>
2022-08-02 23:53:18 +0600

    problem-list, variable

---
+++ variable.js
@@ -0,0 +1,77 @@
+// var x = 10;
+// var x = 15;
+// x = 16;
+// console.log(x);
+
+// let y = 20;
+// y = 22;
+// console.log(y);
+
+// const z = 30;
+// console.log(z);
+/*
+let a = 25;
+let b = 'Snigdha';
+let c = 5 > 4;
+let d = { name: 'Snigdha', age: 23 };
+let e = null;
+let f;
+let g = ['Shamim', 'Snigdha'];
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+
+let myName = 'Shamim';
+console.log(myName);
+
+let x = 20;
+let y = 8;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+// x = x + 5;
+x += 5;
+console.log(x);
+// x = x - 2;
+x -= 3;
+console.log(x);
+// x = x * 2;
+x *= 2;
+console.log(x);
+// x = x / 2;
+x /= 2;
+console.log(x);
+
+// x = x + 1;
+// x += 1;
+x++; // increment
+x--; // decrement
+console.log(x);
+
+
+let p = '50';
+let q = '10.5';
+let sum = Number(p) + Number(q);
+
+let r = 60;
+// let m = r.toString()
+let m = String(r);
+
+
+console.log(typeof m);
+console.log(sum);
+*/
+
+let x = 0.1;
+let y = 0.2;
+let sum = x + y;
+console.log(sum.toFixed(1));
